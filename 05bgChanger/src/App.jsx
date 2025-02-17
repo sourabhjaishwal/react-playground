b6b04665390d69bc589b1d66949ebf8fbec027dd
Sourabh Jaishwal <jaishwalsourabh2000@gmail.com>
2025-02-17 23:45:03 +0530

    bg changer

--- 05bgChanger/src/App.jsx
+++ 05bgChanger/src/App.jsx
@@ -1,11 +1,59 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [color, setColor] = useState("#edede9");
+
+  function changeColor(color) {
+    setColor(color);
+  }
+
   return (
     <>
-      <h1 className="p-4 border-blue-500 bg-blue-300 font-bold text-2xl rounded-2xl">
-        Background Changer Project
-      </h1>
+      <div
+        className="w-full h-screen duration-200"
+        style={{ backgroundColor: color }}
+      >
+        <div className="h-screen flex flex-wrap items-center justify-center shadow-lg">
+          <div className="mb-10 text-3xl text-white font-bold">
+            Background Changer
+          </div>
+          <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+            <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-4 py-4 rounded-full">
+              <button
+                onClick={() => setColor("red")}
+                className="outline-none px-8 shadow-lg py-2 rounded-full bg-red-500 text-white hover:cursor-pointer"
+              >
+                Red
+              </button>
+              <button
+                onClick={() => setColor("blue")}
+                className="outline-none px-8 shadow-lg py-2 rounded-full bg-blue-500 text-white hover:cursor-pointer"
+              >
+                Blue
+              </button>
+              <button
+                onClick={() => setColor("green")}
+                className="outline-none px-8 shadow-lg py-2 rounded-full bg-green-500 text-white hover:cursor-pointer"
+              >
+                Green
+              </button>
+              <button
+                onClick={() => setColor("purple")}
+                className="outline-none px-8 shadow-lg py-2 rounded-full bg-purple-500 text-white hover:cursor-pointer"
+              >
+                Purple
+              </button>
+              <button
+                onClick={() => setColor("orange")}
+                className="outline-none px-8 shadow-lg py-2 rounded-full bg-orange-500 text-white hover:cursor-pointer"
+              >
+                Orange
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
